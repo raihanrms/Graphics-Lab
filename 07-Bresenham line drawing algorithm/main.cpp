dd314427dc6f28bace51b515f3ced5014725a99a
Raihan Munim <raihan.srizon@gmail.com>
2021-10-16 04:07:15 +0600

    added

--- 07-Bresenham line drawing algorithm/main.cpp
+++ 07-Bresenham line drawing algorithm/main.cpp
@@ -71,7 +71,7 @@ int main(int argc, char** argv)
 glutInit(&argc, argv);
 glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
 glutInitWindowSize (500, 500);
-glutInitWindowPosition (400, 400);
+glutInitWindowPosition (100, 100);
 glutCreateWindow ("hello");
 init ();
 glutDisplayFunc(display);
