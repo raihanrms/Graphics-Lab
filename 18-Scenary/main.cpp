291eebbfe95e80d7bffad16299be7a2c69bb01d5
Raihan Munim <raihan.srizon@gmail.com>
2021-11-07 17:21:58 +0600

    added

--- 18-Scenary/main.cpp
+++ 18-Scenary/main.cpp
@@ -1,177 +1,63 @@
-/*
- * GLUT Shapes Demo
- *
- * Written by Nigel Stewart November 2003
- *
- * This program is test harness for the sphere, cone
- * and torus shapes in GLUT.
- *
- * Spinning wireframe and smooth shaded shapes are
- * displayed until the ESC or q key is pressed.  The
- * number of geometry stacks and slices can be adjusted
- * using the + and - keys.
- */
-
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
+#include<windows.h>
 #include <GL/glut.h>
-#endif
 
-#include <stdlib.h>
-
-static int slices = 16;
-static int stacks = 16;
-
-/* GLUT callback Handlers */
-
-static void resize(int width, int height)
+void init(void)
 {
-    const float ar = (float) width / (float) height;
-
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity() ;
+	glClearColor(1.0, 1.0, 1.0, 0.0);	        // Set display window colour to white
+	glMatrixMode(GL_PROJECTION);		        // Set projection parameters
+	gluOrtho2D(0.0, 600.0, 0.0, 600.0);
 }
 
-static void display(void)
+void drawShapes(void)
 {
-    const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-    const double a = t*90.0;
+	glClear(GL_COLOR_BUFFER_BIT);	            // Clear display window
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3d(1,0,0);
+	// background sky
+    glBegin(GL_POLYGON);
+        glColor3ub(245, 183, 177);
+        glVertex3f (0, 400, 0.0);
+        glVertex3f (600, 400, 0.0);
+        glColor3ub(133, 193, 233);
+        glVertex3f (600, 600, 0.0);
+        glVertex3f (0, 600, 0.0);
+    glEnd();
 
-    glPushMatrix();
-        glTranslated(-2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidSphere(1,slices,stacks);
-    glPopMatrix();
+    //  gradient water body
+    glBegin(GL_POLYGON);
+        glColor3ub (36, 113, 163);
+        glVertex3f (0, 200, 0.0);
+        glVertex3f (600, 200, 0.0);
+        glVertex3f (600, 400, 0.0);
+        glColor3ub(133, 193, 233);
+        glVertex3f (0, 400, 0.0);
+    glEnd();
 
-    glPushMatrix();
-        glTranslated(0,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidCone(1,1,slices,stacks);
-    glPopMatrix();
+    // gradient grass
+        glBegin(GL_POLYGON);
+        glColor3ub(11, 83, 69);
+        glVertex3f (0, 0, 0.0);
+        glVertex3f (600, 0, 0.0);
+        glVertex3f (600, 200, 0.0);
+         glColor3ub (46, 204, 113);
+        glVertex3f (0, 200, 0.0);
+    glEnd();
 
-    glPushMatrix();
-        glTranslated(2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
 
-    glPushMatrix();
-        glTranslated(-2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glutSwapBuffers();
+	glFlush();	  // Process all OpenGL routines
 }
 
-
-static void key(unsigned char key, int x, int y)
+int main(int argc, char* argv[])
 {
-    switch (key)
-    {
-        case 27 :
-        case 'q':
-            exit(0);
-            break;
+	glutInit(&argc, argv);					    // Initalise GLUT
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);	// Set display mode
 
-        case '+':
-            slices++;
-            stacks++;
-            break;
+	glutInitWindowPosition(500, 20);			// Set window position
+	glutInitWindowSize(1000, 800);				// Set window size
+	glutCreateWindow("GL Line house");	        // Create display window
 
-        case '-':
-            if (slices>3 && stacks>3)
-            {
-                slices--;
-                stacks--;
-            }
-            break;
-    }
+	init();							            // Execute initialisation procedure
+	glutDisplayFunc(drawShapes);		        // Send graphics to display window
+	glutMainLoop();					            // Display everything and wait
 
-    glutPostRedisplay();
-}
-
-static void idle(void)
-{
-    glutPostRedisplay();
-}
-
-const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
-const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
-
-const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
-const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
-const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat high_shininess[] = { 100.0f };
-
-/* Program entry point */
-
-int main(int argc, char *argv[])
-{
-    glutInit(&argc, argv);
-    glutInitWindowSize(640,480);
-    glutInitWindowPosition(10,10);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-
-    glutCreateWindow("GLUT Shapes");
-
-    glutReshapeFunc(resize);
-    glutDisplayFunc(display);
-    glutKeyboardFunc(key);
-    glutIdleFunc(idle);
-
-    glClearColor(1,1,1,1);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-
-    glEnable(GL_LIGHT0);
-    glEnable(GL_NORMALIZE);
-    glEnable(GL_COLOR_MATERIAL);
-    glEnable(GL_LIGHTING);
-
-    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
-
-    glutMainLoop();
-
-    return EXIT_SUCCESS;
+	return 0;
 }
