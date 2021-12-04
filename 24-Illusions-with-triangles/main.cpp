d8974bea2c3714573b07459d3ccef43a04cca390
Raihan Munim <raihan.srizon@gmail.com>
2021-12-04 13:32:06 +0600

    updated

--- 24-Illusions-with-triangles/main.cpp
+++ 24-Illusions-with-triangles/main.cpp
@@ -1,80 +1,140 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
 
-int triangles(int x,int y,unsigned r,unsigned b,unsigned g)
+/*
+// for same shape triangles, this function can be used
+int triangles(int x,int y)
  {
-     glColor3f(r,b,g);
      // Triangles
      glBegin(GL_TRIANGLES);
-
-        glVertex3f (x-10, y+10 , 0.0);
-        glVertex3f (x+20, y+10, 0.0);
-        glVertex3f (x+20, y+60, 0.0);
-        glVertex3f (x-10, y+60, 0.0);
+     glColor3ub(28, 40, 51);
+        glVertex2i(x+0, y+0);
+        glVertex2i(x+50, y+0);
+        glVertex2i(x+50, y+60);
     glEnd();
-
  }
-int background(int x,int y,unsigned r,unsigned b,unsigned g)
+*/
+
+int background(int x,int y)
  {
-     glColor3f(r,b,g);
-     glBegin(GL_POLYGON);//sky
+     glBegin(GL_POLYGON);
+        glVertex3f (x+0, y+0 , 0.0);
+        glVertex3f (x+500, y+0, 0.0);
+        glVertex3f (x+500, y+600, 0.0);
+        glVertex3f (x+0, y+600, 0.0);
+    glEnd();
+ }
 
-        glVertex3f (x-100, y+20 , 0.0);
-        glVertex3f (x+100, y+20, 0.0);
-        glVertex3f (x+100, y+100, 0.0);
-        glVertex3f (x-100, y+100, 0.0);
+void illusion(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);	// Clear display window
+
+	// T1
+    glBegin(GL_TRIANGLES);
+    glColor3ub(86, 101, 115);
+        glVertex2i(135, 475);
+        glVertex2i(280, 440);
+        glVertex2i(280, 520);
     glEnd();
 
- }
+    // T2
+    glBegin(GL_TRIANGLES);
+    glColor3ub(171, 178, 185);
+        glVertex2i(280, 440);
+        glVertex2i(430, 480);
+        glVertex2i(280, 520);
+    glEnd();
+
+    // T3
+    glBegin(GL_TRIANGLES);
+    glColor3ub(28, 40, 51);
+        glVertex2i(135, 475);
+        glVertex2i(215, 365); // from (215, 380)
+        glVertex2i(280, 440);
+    glEnd();
+
+    // T4
+    glBegin(GL_TRIANGLES);
+    glColor3ub(189, 195, 199);
+        glVertex2i(130, 275);
+        glVertex2i(280, 20);
+        glVertex2i(280, 440);
+    glEnd();
+
+    // T5
+    glBegin(GL_TRIANGLES);
+    glColor3ub(236, 240, 241);
+        glVertex2i(280, 440);
+        glVertex2i(280, 210);
+        glVertex2i(430, 480);
+    glEnd();
+
+    // T6
+    glBegin(GL_TRIANGLES);
+    glColor3ub(28, 40, 51);
+        glVertex2i(280, 220);
+        glVertex2i(430, 270);
+        glVertex2i(370, 370);
+    glEnd();
+
+    // T7
+    glBegin(GL_TRIANGLES);
+    glColor3ub(86, 101, 115);
+        glVertex2i(280, 220);
+        glVertex2i(330, 170);
+        glVertex2i(430, 270);
+    glEnd();
+
+    // T8
+    glBegin(GL_TRIANGLES);
+    glColor3ub(236, 240, 241);
+        glVertex2i(280, 220);
+        glVertex2i(280, 20);
+        glVertex2i(430, 80);
+    glEnd();
+
+    // T9
+    glBegin(GL_TRIANGLES);
+    glColor3ub(28, 40, 51);
+        glVertex2i(130, 90);
+        glVertex2i(280, 20);
+        glVertex2i(180, 190);
+    glEnd();
+
+    glFlush();      // Process all OpenGL routines
+}
 
 void display(void)
 {
 /*  clear all pixels  */
     glClear (GL_COLOR_BUFFER_BIT);
 
-/*  draw white polygon (rectangle) with corners at
- *  (0.25, 0.25, 0.0) and (0.75, 0.75, 0.0)
- */
- //surface
- sky(0,0,.7,.9,1);//sky
- road(0,0,.3,.3,.3);
- grass(0,0,.3,.7,.1);
-
-//#1
- buildings(10,10,0.7,0.7,0.6);
- windows(10,10,1,1,.8);windows(10,20,1,1,.8);windows(10,30,1,1,.8);windows(10,40,1,1,.8);windows(10,50,1,1,.8);
- windows(30,10,1,1,.8);windows(30,20,1,1,.8); windows(30,30,1,1,.8);windows(30,40,1,1,.8);windows(30,50,1,1,.8);
-
- shade(10,10,.8,.8,.7);
-
-//#2
- buildings(50,10,.8,.7,.8);
- windows(50,10,.9,.5,.2);windows(50,20,.9,.5,.2);windows(50,30,.9,.5,.2);windows(50,40,.9,.5,.2);windows(50,50,.9,.5,.2);
- windows(70,10,.9,.5,.2);windows(70,20,.9,.5,.2); windows(70,30,.9,.5,.2);windows(70,40,.9,.5,.2);windows(70,50,.9,.5,.2);
-
-    glFlush ();
+    glColor3ub(195, 155, 211);
+    background(0,0);
+    glFlush();
 }
 
 void init (void)
 {
-/*  select clearing (background) color       */
+/*  select clearing (background) color */
     glClearColor (1.0, 1.0, 1.0, 0.0);
 
 /*  initialize viewing values  */
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(-100.0, 100.0, -100.0, 100.0, -1.0, 1.0);
+    glOrtho(0.0, 500.0, 0.0, 600.0, -1.0, 1.0);
 }
 
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize (650, 750);
-    glutInitWindowPosition (100, 100);
-    glutCreateWindow ("hello");
+    glutInitWindowSize (700, 700);
+    glutInitWindowPosition (500, 100);
+    glutCreateWindow ("Optical Illusion 2");
     init ();
     glutDisplayFunc(display);
+    glutDisplayFunc(illusion);
     glutMainLoop();
     return 0;   /* ISO C requires main to return int. */
 }
